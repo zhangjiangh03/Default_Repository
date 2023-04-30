85d9e7ed06902259380690bfffc776ba13a449a0
zhangjiangh03 <3317229064@qq.com>
2023-04-30 10:43:01 +0800

    first commit

--- Python/pythonProject1/2.py
+++
@@ -1,3 +0,0 @@
-x = 5
-y = "Yes" if x == 5 else "No"
-print(y)
--- Python/pythonProject1/3.py
+++
@@ -1,8 +0,0 @@
-import imageio
-import matplotlib.pyplot as plt
-# 读取图像文件
-image = imageio.imread("1.png")
-# 显示图像
-plt.imshow(image)
-plt.axis("off")
-plt.show()
