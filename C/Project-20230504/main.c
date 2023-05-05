f7728ceed94d9032a7d1a4bd3b1c1146b5dc4450
zhangjiangh03 <3317229064@qq.com>
2023-05-05 18:54:40 +0800

    update

---
+++ C/Project-20230504/main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d*%d=%d\t", i, j, i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Python/PythonApplication-20230504-1.0.0/PythonApplication_20230504_1.0.0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+a = 60
+a = a - 50
+
+print("Hello World", a)
+
+# ע������
+
+"""
+nihao
+"""
+
+name = "world"
+age = "k"
+nessage = "hello %s %s" % (name, age) # 一一对应
+print(nessage)
