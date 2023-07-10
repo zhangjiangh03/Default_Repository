46f2db9fb07bdca36b1936e8044195b5ddd9674b
zhangjiangh03 <3317229064@qq.com>
2023-07-10 16:34:15 +0800

    update

--- Java/Java_FoundationProject_20230706C1_Ver-1.0.0/src/Main.java
+++ Java/Java_FoundationProject_20230706C1_Ver-1.0.0/src/Main.java
@@ -1,17 +1,38 @@
-// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
-// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+import java.util.Scanner;
+
 public class Main {
+    /*
+    * public：这是一个访问修饰符，表示该类是公开的，可以从其他类或包中进行访问。
+    * class：这是关键字，用于声明一个类。
+    * Main：这是类的名称，遵循Java命名规范，类名通常以大写字母开头。
+    *
+    *  class 关键字用于定义类，类是 java 的基本单元
+    */
     public static void main(String[] args) {
-        // 当文本光标位于高亮显示的文本处时按 Alt+Enter，
-        // 可查看 IntelliJ IDEA 对于如何修正该问题的建议。
-        System.out.printf("Hello and welcome!");
+        /*
+        * public：这是一个访问修饰符，表示该方法是公开的，可以从其他类或包中进行访问。
+        * static：这是一个关键字，表示该方法是静态方法，可以直接通过类名来调用，不需要实例化对象。
+        * void：这是方法的返回类型，表示该方法不返回任何值。
+        * main：这是方法的名称，它是一个约定俗成的名称，表示程序的入口点。
+        * String[] args：这是 main 方法的参数列表，它是一个字符串数组。args 是一个约定俗成的名称，用于接收命令行传入的参数。
+        */
+        System.out.println("你好");
+        System.out.println(3.2);
 
-        // 按 Shift+F10 或点击间距中的绿色箭头按钮以运行脚本。
+
+        int test = 60;
+        System.out.println(test);
+
         for (int i = 1; i <= 5; i++) {
-
-            // 按 Shift+F9 开始调试代码。我们已为您设置了一个断点，
-            // 但您始终可以通过按 Ctrl+F8 添加更多断点。
             System.out.println("i = " + i);
         }
+
+        // 创建对象
+        Scanner sc = new Scanner(System.in);
+        // 接受数据
+        int i = sc.nextInt();
+        // 输出数据
+        System.out.println(i);
+
     }
 }
